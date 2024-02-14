c07ff1814500a1072b7a5399a301eadfa60a84fc
penghs <penghs@agilean.cn>
2024-02-14 19:13:55 +0800

    BaseRequest不再实现Serialization接口，因为使用了自定义了序列化器

--- common/src/main/java/cn/nuturbo/common/vo/BaseRequest.java
+++ common/src/main/java/cn/nuturbo/common/vo/BaseRequest.java
@@ -4,13 +4,11 @@ import cn.nuturbo.common.origintype.MemberId;
 import cn.nuturbo.common.utils.Asserts;
 import lombok.Getter;
 
-import java.io.Serializable;
-
 /**
  * Created by penghs at 2024/2/14 6:21
  */
 @Getter
-public abstract class BaseRequest implements Serializable {
+public abstract class BaseRequest {
 
     private final MemberId memberId;
 
