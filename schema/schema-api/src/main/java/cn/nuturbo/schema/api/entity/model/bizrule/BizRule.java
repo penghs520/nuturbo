5d12546d98195600b86f405f039323a06c2f9ebf
penghs <penghs@agilean.cn>
2024-03-07 08:53:24 +0800

    feat：业务规则定义的反序列化支持

--- schema/schema-api/src/main/java/cn/nuturbo/schema/api/entity/model/bizrule/BizRule.java
+++ schema/schema-api/src/main/java/cn/nuturbo/schema/api/entity/model/bizrule/BizRule.java
@@ -4,8 +4,12 @@ import cn.nuturbo.common.origintype.CardTypeId;
 import cn.nuturbo.common.origintype.ConditionId;
 import cn.nuturbo.common.origintype.OrgId;
 import cn.nuturbo.common.origintype.SchemaId;
+import cn.nuturbo.common.utils.Asserts;
 import cn.nuturbo.schema.model.Schema;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
+import org.springframework.lang.Nullable;
 
 import java.util.List;
 
@@ -13,22 +17,28 @@ import java.util.List;
  * TODO 关于更多：1、能不能根据业务规则定义 反向输出业务流程图
  */
 @Getter
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "tag")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PrecheckBizRule.class, name = "PrecheckBizRule"),
+        @JsonSubTypes.Type(value = ReactiveBizRule.class, name = "ReactiveBizRule"),
+})
 public abstract class BizRule extends Schema<BizRuleId> {
 
     // 针对那个卡片类型制定的业务规则
     private final CardTypeId cardTypeId;
 
     // 生效范围，通过过滤来实现
+    @Nullable
     private final ConditionId effectCondition;
 
     // 当发生什么内置动作时触发   注意：不是只看发生了什么事，还要看这件事产生了什么影响或结果  比如回滚事项状态动作，要看具体回滚到哪个状态
     private final ListenEvent listenEvent;
 
-    protected BizRule(BizRuleId id, OrgId orgId, String name, String description, CardTypeId cardTypeId, ConditionId effectCondition, ListenEvent listenEvent) {
+    protected BizRule(BizRuleId id, OrgId orgId, String name, String description, CardTypeId cardTypeId, @Nullable ConditionId effectCondition, ListenEvent listenEvent) {
         super(id, orgId, name, description);
-        this.cardTypeId = cardTypeId;
+        this.cardTypeId = Asserts.notNull(cardTypeId, "cardTypeId is required");
         this.effectCondition = effectCondition;
-        this.listenEvent = listenEvent;
+        this.listenEvent = Asserts.notNull(listenEvent, "listenEvent is required");
     }
 
     @Override
--- schema/schema-api/src/main/java/cn/nuturbo/schema/api/entity/model/bizrule/ReactiveBizRule.java
+++ schema/schema-api/src/main/java/cn/nuturbo/schema/api/entity/model/bizrule/ReactiveBizRule.java
@@ -3,7 +3,10 @@ package cn.nuturbo.schema.api.entity.model.bizrule;
 import cn.nuturbo.common.origintype.CardTypeId;
 import cn.nuturbo.common.origintype.ConditionId;
 import cn.nuturbo.common.origintype.OrgId;
+import cn.nuturbo.common.utils.Asserts;
 import cn.nuturbo.schema.api.entity.model.action.ActionItem;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.util.List;
@@ -20,8 +23,16 @@ public class ReactiveBizRule extends BizRule {
 
     private final List<ActionItem> executeActionItems;
 
-    protected ReactiveBizRule(BizRuleId id, OrgId orgId, String name, String description, CardTypeId cardTypeId, ConditionId effectCondition, ListenEvent listenEvent, List<ActionItem> executeActionItems) {
+    @JsonCreator
+    protected ReactiveBizRule(@JsonProperty("id") BizRuleId id,
+                              @JsonProperty("orgId") OrgId orgId,
+                              @JsonProperty("name") String name,
+                              @JsonProperty("description") String description,
+                              @JsonProperty("cardTypeId") CardTypeId cardTypeId,
+                              @JsonProperty("effectCondition") ConditionId effectCondition,
+                              @JsonProperty("listenEvent") ListenEvent listenEvent,
+                              @JsonProperty("executeActionItems") List<ActionItem> executeActionItems) {
         super(id, orgId, name, description, cardTypeId, effectCondition, listenEvent);
-        this.executeActionItems = executeActionItems;
+        this.executeActionItems = Asserts.notEmpty(executeActionItems, "executeActionItems is required");
     }
 }
